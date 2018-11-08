fb0eb9e5b47b1798a4e1c5352918fe969a007d93
Tinus Flagstad <tinus@flagstad.as>
2018-11-08 19:03:43 +0100

    Wrote queries in front-end so the UI is connected to back-end. #19

--- src/components/MovieGridItem.js
+++ src/components/MovieGridItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
 
 /*
 
@@ -6,10 +8,27 @@ Add a review about a move
 
 */
 class MovieGridItem extends React.Component {
-
-
   render() {
-      return (<div> </div>);
+    const Container = styled.div`
+      cursor: pointer;
+      margin: 30px;
+    `;
+
+    const MoviePoster = styled.img`
+      max-height: 300px;
+      width: 200px;
+      margin-bottom: 0px;
+      border-radius: 4px;
+    `;
+
+    return (
+      <Container>
+        <MoviePoster src={this.props.url} />
+        <Typography style={{ color: "white", margin: 0 }} variant="subtitle1" gutterBottom>
+          {this.props.name}
+        </Typography>
+      </Container>
+    );
   }
 }
 
